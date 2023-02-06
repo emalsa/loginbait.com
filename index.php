362be2ea8f99f6b4421113c88c0c3855277f3a05
dni <daniele@nicastro.io>
2023-02-06 22:31:48 +0100

    remove test

--- index.php
+++ index.php
@@ -37,47 +37,6 @@
 </head>
 <body class="antialiased bg-body text-body font-body">
 <div>
-    <script>
-       alert('navigator.userAgentData.platform: ' + navigator.userAgentData.platform)
-        navigator.getBattery().then((battery) => {
-            function updateAllBatteryInfo() {
-                updateChargeInfo();
-                updateLevelInfo();
-                updateChargingInfo();
-                updateDischargingInfo();
-            }
-            updateAllBatteryInfo();
-
-            battery.addEventListener("chargingchange", () => {
-                updateChargeInfo();
-            });
-            function updateChargeInfo() {
-                alert(`Battery charging? ${battery.charging ? "Yes" : "No"}`);
-            }
-
-            battery.addEventListener("levelchange", () => {
-                updateLevelInfo();
-            });
-            function updateLevelInfo() {
-                alert(`Battery level: ${battery.level * 100}%`);
-            }
-
-            battery.addEventListener("chargingtimechange", () => {
-                updateChargingInfo();
-            });
-            function updateChargingInfo() {
-                alert(`Battery charging time: ${battery.chargingTime} seconds`);
-            }
-
-            battery.addEventListener("dischargingtimechange", () => {
-                updateDischargingInfo();
-            });
-            function updateDischargingInfo() {
-                alert(`Battery discharging time: ${battery.dischargingTime} seconds`);
-            }
-        });
-    </script>
-
     <!-- Navigation/Hero -->
     <section class="overflow-hidden" style="background: url(assets/images/hero/bg_compressed.jpg) no-repeat; background-size: cover;">
         <!-- Navigation -->
